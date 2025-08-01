8b87bfdd7d11b5c102ec78f58da64b1a7ade8fc5
Viktor Gavrilov <Gavr320@yandex.ru>
2025-08-01 23:18:57 +0300

    Реализован Stats-server

--- stats/stats-server/src/main/java/ru/practicum/service/StatsServiceImpl.java
+++ stats/stats-server/src/main/java/ru/practicum/service/StatsServiceImpl.java
@@ -1,7 +1,9 @@
 package ru.practicum.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.EndpointHitDto;
 import ru.practicum.ViewStatsDto;
 import ru.practicum.mapper.StatsMapper;
@@ -14,15 +16,23 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class StatsServiceImpl implements StatsService {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     private final StatsRepository statsRepository;
 
+    @Transactional
     @Override
     public void save(EndpointHitDto endpointHitDto) {
-        EndpointHit hit = StatsMapper.toEntity(endpointHitDto);
-        statsRepository.save(hit);
+        log.debug("Service: Attempting to save hit: {}", endpointHitDto);
+        if (endpointHitDto == null) {
+            log.warn("Service: Cannot save hit, input EndpointHitDto was null.");
+            throw new IllegalArgumentException("Input EndpointHitDto cannot be null.");
+        }
+        EndpointHit endpointHit = StatsMapper.toEntity(endpointHitDto);
+        statsRepository.save(endpointHit);
+        log.info("Service: Hit saved successfully for app: {}, uri: {}", endpointHit.getApp(), endpointHit.getUri());
     }
 
     @Override
