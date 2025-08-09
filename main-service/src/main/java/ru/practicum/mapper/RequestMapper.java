c7103b41dd37692f93023d3d39eaf7397dce05fb
Alexander Kosyanin <kosyanin@yandex.ru>
2025-08-09 18:08:03 +0300

    Реализована сущность Request, соответствующие маппер и репозиторий

---
+++ main-service/src/main/java/ru/practicum/mapper/RequestMapper.java
@@ -0,0 +1,14 @@
+package ru.practicum.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import ru.practicum.dto.request.ParticipationRequestDto;
+import ru.practicum.model.Request;
+
+@Mapper(componentModel = "spring", uses = {UserMapper.class})
+public interface RequestMapper {
+
+    @Mapping(target = "requesterId", source = "request.requester.id")
+    @Mapping(target = "eventId", source = "request.event.id")
+    ParticipationRequestDto toDto(Request request);
+}
